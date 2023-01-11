69dc80e2e10fc74e4b88cef9ed1fd5b4d0ebb1bb
sk4ndal-commits <73082008+sk4ndal-commits@users.noreply.github.com>
2023-01-11 18:21:26 +0100

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '0oa3rl60vyLq8hXbY697',
-    issuer: 'https://trial-4018962.okta.com/oauth2/default',
+    clientId: '',
+    issuer: '',
     redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid','profile','email']
   }
