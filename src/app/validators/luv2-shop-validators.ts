e49ffe10b918f0e0dacb41c0630deba26a9aa088
sascha-roggatz <sascharoggatz1999@outlook.de>
2025-10-08 01:34:18 +0200

    Refactor notOnlyWhitespace validator for improved readability

--- src/app/validators/luv2-shop-validators.ts
+++ src/app/validators/luv2-shop-validators.ts
@@ -5,7 +5,7 @@ export class Luv2ShopValidators {
   static notOnlyWhitespace(control: FormControl): ValidationErrors | null {
     // @ts-ignore
     return (control.value != null) && (control.value.trim().length === 0)
-    ? {'notOnlyWhitespace': true}
-    : null;
+      ? {'notOnlyWhitespace': true}
+      : null;
   }
 }
